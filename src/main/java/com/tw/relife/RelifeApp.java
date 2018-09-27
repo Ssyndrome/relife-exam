9cf417ffbe3887c751a93d081bd7b72cf3c410cc
Ssyndrome <915448715@qq.com>
2018-09-27 10:48:37 +0800

    [Syndrome][1] full the app implemention

--- src/main/java/com/tw/relife/RelifeApp.java
+++ src/main/java/com/tw/relife/RelifeApp.java
@@ -7,12 +7,22 @@ public class RelifeApp implements RelifeAppHandler {
 
     public RelifeApp(RelifeAppHandler handler) {
         // TODO: You can start here
-        throw new NotImplementedException();
+        if (handler == null) {
+            throw new IllegalArgumentException();
+        }
+        this.handler = handler;
     }
 
     @Override
     public RelifeResponse process(RelifeRequest request) {
         // TODO: You can start here
-        throw new NotImplementedException();
+        try {
+            handler.process(request);
+        } catch (Exception exception) {
+            RelifeResponse response = new RelifeResponse(500);
+            return response;
+        }
+        RelifeResponse response = new RelifeResponse(200);
+        return response;
     }
 }
